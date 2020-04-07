96737fdc0fb930fdeb690375b8e6ed3a2933259f
Enmanuel Rivas <Enmrivas2301@gmail.com>
2020-04-06 21:59:17 -0400

    agregado funcion login admin

---
+++ index.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2thdGVvaGFsaWMxNTA0IiwiYSI6ImNrOG5jbWZjMTBzbXkzbXBzNXc2ajNyaXcifQ.9cboPzPcoNeS-CllveacJg';
+			var map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11'
+			});
---
+++ login.php
@@ -0,0 +1,66 @@
+<?php include('functions.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration system PHP and MySQL</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="styleIndex.css">
+	<!-- Mapbox -->
+	<script src='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js'></script>
+	<link href='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css' rel='stylesheet' />
+</head>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+<header>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	  <a class="navbar-brand" href="index.html">COVID-19</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="index.html">Home</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="login.php">Login</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="register.php">Registrate hoy!</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="Carpeta About Us - Proyecto Final/index.html">Sobre Nosotros</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	</header>
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	<form method="post" action="login.php">
+
+		<?php echo display_error(); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_btn">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
+</body>
+</html>
