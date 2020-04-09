917d6007136254a3cc2b4b1d857ae56c7ec2e545
Enmrivas <60750259+Enmrivas@users.noreply.github.com>
2020-04-09 00:48:15 -0400

    agregado el api de telegram

--- save.php
+++ save.php
@@ -18,6 +18,15 @@
   if($query) {
     echo "<script> alert('Correcto');
       location.href ='home.php'; </script>";
+
+      $apiToken = "1116214164:AAGXVR3EmiiQ-3Vz8ScAN4ApJ2wZrma9CNw";
+
+      $data = [
+          'chat_id' => '-477785739',
+          'text' => "Un nuevo caso ha sido descubierto en la ciudad de: ".$Ciudad
+      ];
+
+      $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
     
   } else{
     echo "<script> alert('Incorrecto'); 
